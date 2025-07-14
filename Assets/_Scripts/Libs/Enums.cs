ff9bc0f9bf66a0ab4b2876aa7e228751d4e3e24a
Ashures <ashuresyt@gmail.com>
2025-07-14 16:34:18 +1200

    feat: Create ScriptableObject for building data

---
+++ Assets/_Scripts/Libs/Enums.cs
@@ -0,0 +1,13 @@
+public enum RPLevels
+{
+  COMMON = 0,
+  UNCOMMON = 1,
+  RARE = 2,
+  SUPER_RARE = 3,
+  UNIQUE = 4,
+  EPIC = 5,
+  LEGENDARY = 6,
+  MYTHIC = 7,
+  DIVINE = 8,
+  ALMIGHTY = 9,
+}
---
+++ Assets/_Scripts/ScriptableObjects/BuildingInfo.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "BuildingInfo", menuName = "ScriptableObjects/BuildingInfo")]
+public class BuildingInfo : ScriptableObject
+{
+  public GameObject prefab;
+
+  public string displayName;
+  public Sprite sprite;
+  public string desc;
+  public int price;
+  public RPLevels tier;
+}
